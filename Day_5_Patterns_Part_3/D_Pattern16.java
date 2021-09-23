7a873480ca4988c2f759804cd71a74418cad26bd
Sanjay Chhabria <sonu61989@gmail.com>
2021-09-23 16:01:04 +0530

    Home Work Added Patterns 13,15,16,18

--- Day_5_Patterns_Part_3/D_Pattern16.java
+++ Day_5_Patterns_Part_3/D_Pattern16.java
@@ -2,18 +2,39 @@ package Day_5_Patterns_Part_3;
 
 import java.util.*;
 
-public class D_Pattern16  {
+public class D_Pattern16 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int sop = (2 * n) - 3;
+		int val = 1;
+		for (int i = 1; i <= n; i++) {
+			val = 1;
+			for (int j = 1; j <= i; j++) {
+				System.out.print(val + "\t");
+				val = val + 1;
+			}
+			for (int j = 1; j <= sop; j++) {
+				System.out.print("\t");
+			}
+			int k = (i == n) ? i - 1 : i;
+			val = k;
+			for (int j = 1; j <= k; j++) {
+				System.out.print(val + "\t");
+				val--;
+			}
+			sop = sop - 2;
+			System.out.println();
+		}
 	}
 }
-
 
 /*
 Sample Input
 7
 
 Sample Output
+for n=7
 1												1	
 1	2										2	1	
 1	2	3								3	2	1	
@@ -22,4 +43,9 @@ Sample Output
 1	2	3	4	5	6		6	5	4	3	2	1	
 1	2	3	4	5	6	7	6	5	4	3	2	1	
 
+for n=5
+1						1	
+1	2				2	1	
+1	2	3		3	2	1	
+1	2	3	4	3	2	1	
 */
